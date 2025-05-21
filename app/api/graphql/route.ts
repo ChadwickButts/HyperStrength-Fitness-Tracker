0758ea6b24dd70cadd8e36495b7bc154822f9302
Chadwick Butts <ccbutts@mail.usf.edu>
2025-05-21 13:31:02 -0400

    adds graphql

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,51 @@
+// https://www.npmjs.com/package/@as-integrations/next
+
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next'
+import { NextRequest } from 'next/server';
+import postgres from 'postgres';
+import { Resolvers } from '../../../types';
+import { exerciseData } from '@/app/lib/definitions';
+
+const resolvers = {
+    Query: {
+        getExercises: async (parent: any, args: any, contextValue: any ) => { 
+            return await contextValue.db<exerciseData[]>`SELECT * FROM public.exercises`
+        }
+    }
+}
+
+const typeDefs = `#graphql
+    type Exercise {
+        exercisename: String
+        force: String 
+        level: String 
+        mechanic: String 
+        equipment: String 
+        primaryMuscles: [String] 
+        secondaryMuscles: [String] 
+        instructions: [String] 
+        category: String 
+        images: [String] 
+        exerciseid: String
+    }
+
+    type Query {
+        getExercises: [Exercise]
+    }
+`;
+
+const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+});
+
+// https://www.apollographql.com/docs/apollo-server/v3/data/resolvers#the-context-argument
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+    context: async (req: any) => ({ 
+        req,
+        db: postgres(process.env.PG_URL!)
+    }),
+});
+
+export { handler as GET, handler as POST }
