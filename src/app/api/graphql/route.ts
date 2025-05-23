044579bc641e365237f7b17bd588ede131fe889a
Chadwick Butts <ccbutts@mail.usf.edu>
2025-05-23 01:40:40 -0400

    changed env variable back

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -15,7 +15,7 @@ const server = new ApolloServer({
 // https://www.apollographql.com/docs/apollo-server/v3/data/resolvers#the-context-argument
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
     context: async (req, res) => ({
-        db: await postgres(process.env.NEXT_PUBLIC_PG_URL!)
+        db: await postgres(process.env.PG_URL!)
     })
 });
 
