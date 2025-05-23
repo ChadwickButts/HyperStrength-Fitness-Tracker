b3420696299471f5ed717c67467786ff560807fe
Chadwick Butts <ccbutts@mail.usf.edu>
2025-05-23 01:03:39 -0400

    corrected header

--- src/app/apollowrapper.tsx
+++ src/app/apollowrapper.tsx
@@ -9,7 +9,7 @@ export function ApolloWrapper({ children }: { children: React.ReactNode }) {
     const client = new ApolloClient({
         uri: 'http://localhost:3000/api/graphql',
         headers: { 
-            'Allow-Control-Access-Origin': '*'
+            'Access-Control-Allow-Origin': '*'
         },
         cache: new InMemoryCache()
     });
